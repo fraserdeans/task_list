102c23f4929b44930a3496ea32207683c47fe65e
Fraser Deans <fraser@shutl.co.uk>
2011-07-29 14:11:09 +0100

    Add javascript effects

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function closeMessages(){
+  $('.success').fadeOut('slow');
+}
+
+$(document).ready(function(){
+ setTimeout('closeMessages()', 1000);
+})
